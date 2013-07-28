57f67e82aecc771425eb30f2d73495c17126121c
Ben Jennings <ben@protagonist.co.uk>
2013-07-28 11:52:10 +0100

    Added per feed author muting
    Added a new function, authormute, which gives the ability to mute
    individuals authors in one feed. This is useful in feeds from big
    sites, TheVerge for example, where you only want to read content from
    certain authors.
    
    Additionally, rather than triggering the fever-filter script on a cron,
    it is possible to hook the main Fever refresh script. The location is:
    fever/firewall/app/libs/fever.php, line 3311 at the end of the function
    called Infiniterator. If you call the fever-filter script there, it
    gets called only once after Fever has finished.

--- fever-filter.php
+++ fever-filter.php
@@ -4,12 +4,13 @@
 	require_once("functions/fun.filter.php");
 
 	// filter("Feed Title",array("Words in","The Title"));
+	// authormute("Feed Title",array("author 1","author 2"));
 
 	filter("*",array(
 							"Sponsor:",
 							"[Sponsor]"
 	));
-	
+
 	filter("Penny Arcade",array("News Post:"));
 
 ?>
--- functions/fun.filter.php
+++ functions/fun.filter.php
@@ -42,4 +42,34 @@
 		}
 
 	}
+
+	function authormute($feed,$authors) {
+		global $db;
+
+		$items = $db->get_results("
+			SELECT fever_items.id, fever_items.feed_id, fever_items.author, fever_items.added_on_time
+			FROM fever_items
+			LEFT OUTER JOIN fever_feeds ON fever_feeds.id = fever_items.feed_id
+			WHERE fever_items.read_on_time = '0' AND fever_feeds.title = '$feed';
+			");
+			if (count($items) > 0)
+			{
+				foreach ($items as $item)
+				{
+					$itemID = $item->id;
+					foreach ($authors as $author)
+					{
+						if (is_int(strpos(strtolower($item->author), strtolower($author))))
+						{
+    						$result = $db->get_results("
+    							UPDATE fever_items
+    							SET fever_items.read_on_time = fever_items.added_on_time
+    							WHERE fever_items.id = '$itemID';
+    						");
+							break 1;
+						}
+					}
+				}
+			}
+	}
 ?>
